2ee15a7cddeae1915da241cf3c6d975a11caecd7
Alyosha-ctrl <alexeibere@gmail.com>
2024-10-25 15:15:34 -0700

    Add files via upload

---
+++ cst205_lab14Git.py
@@ -0,0 +1,26 @@
+#Task 1 Install Git and GitHub
+#I cannot do this because my computer does not have enough space to fit Git and Github command line stuff
+#All the space that's left is required. 
+#Although frankly some command line stuff being 20 gigs is goshdarn ridiculous in the first place
+#I can barely spare a single gig much less 20!
+
+#Task 2
+#If I were to install a new git repository I would do it like this.
+#echo "# lab11" >> README.md
+#git init
+#git add README.md
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/Alyosha-ctrl/lab11.git
+#git push -u origin main
+
+#I will still put the file there though not through command line
+
+
+#Task 3
+from listStore import my_list
+print(my_list)
+
+#Task 4
+#I got to ___
+
